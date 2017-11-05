ec2b7fa52fbaa96f0445d94fd6dac2db2de9929a
mkucharczyk <maciej.kucharczyk@hotmail.com>
2017-11-05 13:52:32 +0100

    example config file added

---
+++ include/user_config.h
@@ -0,0 +1,17 @@
+#ifndef __USER_CONFIG_H__
+#define __USER_CONFIG_H__
+
+
+#define WIFI_SSID "YOUR-SSID"
+
+#define WIFI_PASSWORD "YOUR-PASSWORD"
+
+#define WS_SERVER "YOUR-SERVER-IP-OR-HOST"
+
+#define WS_PORT 1337
+
+#define WS_PATH "/"
+
+#endif
+
+
